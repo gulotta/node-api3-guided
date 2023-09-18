43d41180fa43fd84c5e8bcd094c2d32aeb0242db
Michael Gulotta <gulottam@gmail.com>
2023-09-18 10:21:53 -0400

    complete

---
+++ api/hubs/hubs-middleware.js
@@ -0,0 +1,36 @@
+const Hubs = require('./hubs-model.js');
+
+async function checkHubId(req, res, next) {
+    try {
+        const hub = await Hubs.findById(req.params.id)
+        if(hub) {
+            req.hub = hub
+            next()
+        } else {
+            next({status: 404, message: `Hub ${req.params.id} not found`})
+        }
+    } catch (err) {
+        next(err)
+    }
+}
+
+function checkNewHub (req, res, next) {
+    const {name} = req.body;
+    if (
+        name !== undefined && 
+        typeof name === "string" && 
+        name.length
+        ){
+            next()
+    } else {
+        res.status(422).json({ 
+            message: 'hubs need a name'
+        })
+    }
+}
+
+module.exports = {
+
+    checkHubId,
+    checkNewHub
+}
